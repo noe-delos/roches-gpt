eea60d1ea70552bcdcbcb501f228d839b756b447
Noé Campo <noecampo@delosintelligence.fr>
2025-04-08 22:45:49 +0200

    new docs feature

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { HeroUIProvider } from "@heroui/react";
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
+import { Suspense } from "react";
+import "./globals.css";
+
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
+  variable: "--font-ubuntu",
+});
+
+export const metadata: Metadata = {
+  title: "ROCHES-gpt",
+  description: "L'assistant alumni IA de l'ROCHES",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={`${ubuntu.className} bg-white`}>
+        <Analytics />
+        <HeroUIProvider>
+          <Suspense> {children}</Suspense>
+        </HeroUIProvider>
+      </body>
+    </html>
+  );
+}
